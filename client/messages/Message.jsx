b327abe17865116b975d7328f2d0efcf3f03d9cb
Gilles Manzato <gilles.manzato@gmail.com>
2016-04-04 00:43:44 +0800

    Add username and date to the message

--- client/messages/Message.jsx
+++ client/messages/Message.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import Moment from 'moment';
+
 
 export default class Message extends React.Component {
+	renderUsername() {
+		const username = Meteor.users.findOne(this.props.message.userId).username;
+		return (
+			<span>{username} </span>
+		)
+	}
+
+	renderDate() {
+		const parsedDate = Moment(this.props.message.createdAt).format('hh:mm a');
+		return (
+			<span>{parsedDate} </span>
+		)
+
+	}
 	render() {
 		return (
-			<li className="Message">{this.props.message.text}</li>
+			<li className="Message">
+				{this.renderDate()	}
+				{this.renderUsername()	}
+				{this.props.message.text}
+			</li>
 		)
 	}
 }
--- server/collections/messages.js
+++ server/collections/messages.js
@@ -3,7 +3,28 @@ import { Meteor } from 'meteor/meteor';
 import Messages from '/lib/collections/messages.js'
 import Teams from '/lib/collections/teams.js'
 
-Meteor.publish('messages', function(team,room){
-	const inTeam = Teams.find({ _id : team , 'users.user' : this.userId });
-	return inTeam.count() ? Messages.find({room_id : room}) : [];
+// Meteor.publishComposite('messages', {
+// 	find: function(team,room) {
+// 		const inTeam = Teams.find({ _id : team , 'users.user' : this.userId });
+		
+// 	}
+// });
+
+Meteor.publishComposite('messages', function(teamId,roomId) {
+
+  return {
+    find : function() {
+    	const inTeam = Teams.find({ _id : teamId , 'users.user' : this.userId });
+		return Messages.find({room_id : roomId});
+    },
+
+    children: [{
+      find(message) {
+        return Meteor.users.find({_id : message.userId},{fields: {
+  			_id : 1,
+  			username : 1
+  		}})
+      }
+    }]
+  };
 });
